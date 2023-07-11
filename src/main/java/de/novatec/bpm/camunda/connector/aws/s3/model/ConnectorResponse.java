bc48a0fc3d1995cf07ccd40887c060edba12f8e1
Isaias Armoa <isaias.armoa@gmail.com>
2023-07-11 15:50:28 +0100

    Support for Lombok version 1.18.28
    https://github.com/NovatecConsulting/camunda-aws-s3-connector/issues/8

--- src/main/java/de/novatec/bpm/camunda/connector/aws/s3/model/ConnectorResponse.java
+++ src/main/java/de/novatec/bpm/camunda/connector/aws/s3/model/ConnectorResponse.java
@@ -1,9 +1,13 @@
 package de.novatec.bpm.camunda.connector.aws.s3.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import java.util.Objects;
 
+@Data
+@NoArgsConstructor
 public class ConnectorResponse {
   private String serverSideEncryption;
   private String versionId;
@@ -13,43 +17,4 @@ public class ConnectorResponse {
     this.versionId = response.versionId();
   }
 
-  public ConnectorResponse() {
-  }
-
-  public String getServerSideEncryption() {
-    return serverSideEncryption;
-  }
-
-  public void setServerSideEncryption(String serverSideEncryption) {
-    this.serverSideEncryption = serverSideEncryption;
-  }
-
-  public String getVersionId() {
-    return versionId;
-  }
-
-  public void setVersionId(String versionId) {
-    this.versionId = versionId;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    ConnectorResponse that = (ConnectorResponse) o;
-    return Objects.equals(serverSideEncryption, that.serverSideEncryption) && Objects.equals(versionId, that.versionId);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(serverSideEncryption, versionId);
-  }
-
-  @Override
-  public String toString() {
-    return "ConnectorResponse{" +
-            "serverSideEncryption='" + serverSideEncryption + '\'' +
-            ", versionId='" + versionId + '\'' +
-            '}';
-  }
 }
